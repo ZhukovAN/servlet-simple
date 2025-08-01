c8104715c4548db745444f129cd5cc9c4f1e4a9a
Alexey N. Zhukov <alexey.n.zhukov@gmail.com>
2025-08-01 11:07:35 +0300

    Cross-functional SQLi added

--- src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
+++ src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
@@ -6,6 +6,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 @WebServlet("/simple")
 public class Servlet extends HttpServlet {
@@ -14,6 +16,12 @@ public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) {
         String cmd = request.getParameter("cmd");
+        if (LocalDate.now().getDayOfWeek().equals(DayOfWeek.FRIDAY))
+            cmd = process(cmd);
         Runtime.getRuntime().exec(cmd);
+    }
+
+    protected String process(String cmd) {
+        return cmd.toLowerCase();
     }
 }
