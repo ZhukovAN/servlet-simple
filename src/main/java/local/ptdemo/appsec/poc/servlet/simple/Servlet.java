c0a6b23bfd7e8dc60ff4eae8dd3947f89b6db38d
Alexey N. Zhukov <alexey.n.zhukov@gmail.com>
2026-07-15 13:20:29 +0000

    Merge commit '385c11f65df2b18fa8ab8ee3c398c83582a11716'

--- src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
+++ src/main/java/local/ptdemo/appsec/poc/servlet/simple/Servlet.java
@@ -6,14 +6,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
 
 @WebServlet("/simple")
 public class Servlet extends HttpServlet {
-    @SneakyThrows
+    public static final Random PRNG = new Random();
+    public static final long RANDOM = PRNG.nextLong();
+
     @Override
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) {
-        String cmd = request.getParameter("cmd");
-        Runtime.getRuntime().exec(cmd);
     }
 }
